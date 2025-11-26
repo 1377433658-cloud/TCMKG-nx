da4ef32cfb990f0d87a516e1e774f6eb2f364a8f
1377433658-cloud <1377433658@qq.com>
2025-11-26 15:06:24 +0800

    Delete vite.config.ts

--- vite.config.ts
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'  // 新增这一行
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react(), tailwindcss()],  // 在 plugins 里加 tailwindcss()
-})
